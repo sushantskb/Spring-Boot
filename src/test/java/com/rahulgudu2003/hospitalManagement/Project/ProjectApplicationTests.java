05c1d430785c612f790585fc298f545ba8a7087f
infokarrierlang <sushantsrk20012gmail.com>
2026-03-19 09:37:20 +0530

    Initial commit for project setup

---
+++ src/test/java/com/rahulgudu2003/hospitalManagement/Project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahulgudu2003.hospitalManagement.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
