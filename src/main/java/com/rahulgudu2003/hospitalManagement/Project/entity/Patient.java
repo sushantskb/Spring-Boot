df51d5901b8a016793bb1fd1fd32ee0550fe0432
Sushant Kumar Bishoi <91365425+sushantskb@users.noreply.github.com>
2026-03-25 11:18:56 +0000

    entity, table and orm

--- src/main/java/com/rahulgudu2003/hospitalManagement/Project/entity/Patient.java
+++ src/main/java/com/rahulgudu2003/hospitalManagement/Project/entity/Patient.java
@@ -1,27 +1,78 @@
 package com.rahulgudu2003.hospitalManagement.Project.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.rahulgudu2003.hospitalManagement.Project.entity.type.BloodGroupType;
 
 @Entity
 @ToString
 @Getter
 @Setter
+@Table(name = "patient", uniqueConstraints = {
+        // @UniqueConstraint(name = "unique_patient_email", columnNames = { "email" }),
+        // // not makes sense since only one column is there
+        @UniqueConstraint(name = "unique_patient_name_birthdate", columnNames = { "name", "birthDate" }) // makes more
+                                                                                                         // sense for
+                                                                                                         // unique
+                                                                                                         // constraints
+}, indexes = {
+        @Index(name = "idx_patient_birth_date", columnList = "birthDate")
+})
+// @Table(name = "patient_tbl", uniqueConstraints = {
+// // @UniqueConstraint(name = "unique_patient_email", columnNames = { "email"
+// }),
+// // // not makes sense since only one column is there
+// @UniqueConstraint(name = "unique_patient_name_birthdate", columnNames = {
+// "name", "birthDate" }) // makes more
+// // sense for
+// // unique
+// // constraints
+// }, indexes = {
+// @Index(name = "idx_patient_birth_date", columnList = "birthDate")
+// }) // just for understanding, we can also use @Table annotation to define
+// unique constraints and indexes at the table level instead of column level
 public class Patient {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // @Column(name = "patient_name", nullable = false, length = 40) // just for
+    // understanding we can also use @Column annotation to define column name,
+    // nullable and length constraints at the column level instead of table level
+    @Column(nullable = false, length = 40)
     private String name;
     private LocalDate birthDate;
+
+    @Column(unique = true, nullable = false)
     private String email;
     @ToString.Exclude
     private String gender;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(updatable = true)
+    private LocalDateTime updatedTime;
+
+    @Enumerated(EnumType.STRING)
+    private BloodGroupType bloodGroup;
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.patient (birth_date, email, gender, name, blood_group)
+VALUES 
+('1990-01-15', 'patient1@example.com', 'Male', 'John Doe', 'A_POSITIVE'),
+('1985-05-22', 'patient2@example.com', 'Female', 'Jane Smith', 'B_NEGATIVE'),
+('1978-11-30', 'patient3@example.com', 'Male', 'Robert Johnson', 'AB_POSITIVE'),
+('1992-08-10', 'patient4@example.com', 'Female', 'Emily Brown', 'O_NEGATIVE'),
+('1980-03-25', 'patient5@example.com', 'Male', 'Michael Wilson', 'A_NEGATIVE');
